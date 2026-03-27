5e251e8d88ab2b2d4b7db29b35ccd2ccb312bf93
박현우 <hyuns@hyuns.dev>
2026-03-27 23:30:02 +0900

    fix: pm2 파일 수정

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,12 +3,15 @@ module.exports = {
     {
       name: "e-ink-server",
       cwd: __dirname,
-      script: "dist/server.js",
-      interpreter: "node",
+      script: "pnpm",
+      args: "start",
+      interpreter: "none",
       instances: 1,
       exec_mode: "fork",
       autorestart: true,
       watch: false,
+      time: true,
+      restart_delay: 1000,
       env: {
         NODE_ENV: "production",
         PORT: process.env.PORT || "3000",
